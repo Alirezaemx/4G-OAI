ce9c183839dd4bfaa972fc950e46195a911120d6
Alireza <122540395+Alirezaemx@users.noreply.github.com>
2023-10-31 10:14:52 +0330

    Add files via upload

---
+++ get.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "Pull Docker images:"
+docker pull cassandra:2.1
+docker pull redis:6.0.5
+docker pull oaisoftwarealliance/oai-hss:latest
+docker pull oaisoftwarealliance/magma-mme:latest
+docker pull oaisoftwarealliance/oai-spgwc:latest
+docker pull oaisoftwarealliance/oai-spgwu-tiny:latest
+docker pull oaisoftwarealliance/trf-gen-cn5g:latest
+docker pull oaisoftwarealliance/oai-enb:develop
+docker pull oaisoftwarealliance/oai-lte-ue:develop
+
+#re-tag
+#docker image tag oaisoftwarealliance/oai-spgwc:latest oai-spgwc:latest
+#docker image tag oaisoftwarealliance/oai-hss:latest oai-hss:latest
+#docker image tag oaisoftwarealliance/oai-spgwu-tiny:latest oai-spgwu-tiny:latest 
+#docker image tag oaisoftwarealliance/magma-mme:latest magma-mme:latest
+
+#echo "Done"

3a7f07b8124b4c9d2a7a8d4d5e40d11708d0666e
Alireza <122540395+Alirezaemx@users.noreply.github.com>
2023-11-03 11:19:10 +0330

    Update core.sh

--- core.sh
+++ core.sh
@@ -4,6 +4,7 @@ sleep 20
 docker logs rfsim4g-db-init --follow
 sleep 5
 docker rm rfsim4g-db-init
+nohup tshark -i rfsim4g-public -f 'port 3868 or port 2123 or port 36412 or port 36422 or port 46520 or port 8805' -w /tmp/4G-OAI.pcap
 docker-compose up -d magma_mme oai_spgwu trf_gen
 sleep 15
 docker ps -a

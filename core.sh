4d1306c188fc2aa9efbe89585d6f80028eae6a81
Alireza <122540395+Alirezaemx@users.noreply.github.com>
2023-11-04 08:49:58 +0330

    Update core.sh

--- core.sh
+++ core.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+BLUE='\033[0;34m'
+echo "${BLUE}It will end in 1 minute!"
 docker-compose up -d db_init
 sleep 20
-docker logs rfsim4g-db-init --follow
+docker logs db-init --follow
 sleep 5
-docker rm rfsim4g-db-init
+docker rm db-init
 docker-compose up -d magma_mme oai_spgwu trf_gen
 sleep 15
 docker ps -a
-sleep 10
-docker exec rfsim4g-magma-mme /bin/bash -c "cat /var/log/mme.log"
+sleep 20
+docker exec magma-mme /bin/bash -c "cat /var/log/mme.log"
+echo "${BLUE}End!"

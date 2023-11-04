d14522902ae53d499c25d05c049582534d4a4f96
Alireza <122540395+Alirezaemx@users.noreply.github.com>
2023-11-04 09:14:28 +0330

    Update

--- get.sh
+++ get.sh
@@ -7,8 +7,8 @@ docker pull oaisoftwarealliance/magma-mme:latest
 docker pull oaisoftwarealliance/oai-spgwc:latest
 docker pull oaisoftwarealliance/oai-spgwu-tiny:latest
 docker pull oaisoftwarealliance/trf-gen-cn5g:latest
-docker pull oaisoftwarealliance/oai-enb:develop
-docker pull oaisoftwarealliance/oai-lte-ue:develop
+docker pull oaisoftwarealliance/oai-enb:2022.w46
+docker pull oaisoftwarealliance/oai-lte-ue:2022.w46
 
 #re-tag
 #docker image tag oaisoftwarealliance/oai-spgwc:latest oai-spgwc:latest

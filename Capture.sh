5c15dc35b4d300e86e77b3f8da57f0b9c439f8d6
Alireza <122540395+Alirezaemx@users.noreply.github.com>
2023-11-12 17:00:40 +0330

    Update

--- Capture.sh
+++ Capture.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-tshark -i rfsim4g-public -f 'port 3868 or port 2123 or port 36412 or port 36422 or port 46520 or port 8805' -w /tmp/4G-OAI.pcap
+tshark -i public -f 'port 3868 or port 2123 or port 36412 or port 36422 or port 46520 or port 8805' -w /tmp/4G-OAI.pcap
 echo "Your PCAP File in /tmp/4G-OAI.pcap"
